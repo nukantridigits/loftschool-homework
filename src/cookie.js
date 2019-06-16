e144edc802557fa5d057591e51f882ce2841357c
anozhenko <a.nozhenko@andagar.ru>
2019-06-16 05:10:13 +0300

    homework_7:

--- src/cookie.js
+++ src/cookie.js
@@ -43,11 +43,113 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
+const loadCookies = () => {
+    console.log('loadCookies');
+    let cookies = document.cookie;
+    if (cookies.length) {
+        return cookies.split('; ').reduce((prev, current) => {
+            const [name, value] = current.split('=');
+
+            prev[name] = value;
+
+            return prev;
+        }, {});
+    }
+
+    return {};
+};
+
+const getCookies = () => {
+    console.log('getCookies');
+    clearList(listTable);
+    let cookies = loadCookies();
+
+    if (Object.keys(cookies).length !== 0) {
+        for (let cookie in cookies) {
+            if (cookies.hasOwnProperty(cookie)) {
+                createTr(cookie, cookies[cookie]);
+            }
+        }
+    }
+
+    return true;
+};
+
+const createTr = (cookie, cookieValue) => {
+    console.log('createTr');
+    const deleteText = 'Удалить cookie';
+    let tr = document.createElement('tr');
+    let name = document.createElement('td');
+    let value = document.createElement('td');
+    let deleteCell = document.createElement('td');
+    let cookieDeleteBtn = document.createElement('button');
+
+    name.textContent = cookie;
+    value.textContent = cookieValue;
+    cookieDeleteBtn.classList.add('cookie-delete-btn');
+    cookieDeleteBtn.textContent = deleteText;
+    deleteCell.appendChild(cookieDeleteBtn);
+
+    listTable.appendChild(tr);
+    for (let cell of [name, value, deleteCell]) {
+        tr.appendChild(cell);
+    }
+
+    return true;
+};
+
+document.addEventListener('click', event => {
+    let target = event.target;
+
+    if (target.classList.contains('cookie-delete-btn')) {
+        let cookieName = target.parentNode.previousSibling.previousSibling.textContent;
+        let tr = target.closest('tr');
+
+        console.log('cookie for delete', cookieName);
+
+        return deleteCookie(cookieName, tr);
+    }
+});
+
+const setCookie = (name, value, days) => {
+    console.log('setCookie');
+    let date = new Date();
+
+    date.setDate(date.getDate() + days);
+    document.cookie = name + '=' + value + ';path=/; expires=' + date.toUTCString();
+
+    return getCookies();
+};
+
+const clearList = element => {
+    console.log('clearList');
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+};
+
+const deleteCookie = (name, tr) => {
+    console.log('deleteCookie');
+    listTable.removeChild(tr);
+    setCookie(name, '', -1);
+};
+
+getCookies();
+
 filterNameInput.addEventListener('keyup', function() {
     // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
 });
 
 addButton.addEventListener('click', () => {
     // здесь можно обработать нажатие на кнопку "добавить cookie"
+    let cookieName = addNameInput.value.trim();
+    let cookieValue = addValueInput.value.trim();
+
+    if (cookieName.length && cookieValue.length) {
+        setCookie(cookieName, cookieValue, 14);
+        for (let input of [... document.querySelectorAll('#add-block input')]) {
+            input.value = '';
+        }
+    }
 });
 
